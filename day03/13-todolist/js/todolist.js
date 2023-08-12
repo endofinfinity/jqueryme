92ad9fdbe71a4eddf41b9f2e50128690ddcd79d8
mailab <z1563492018@gmail.com>
2023-08-12 23:32:17 +0800

    pull done

--- day03/13-todolist/js/todolist.js
+++ day03/13-todolist/js/todolist.js
@@ -1,7 +1,7 @@
-$(function () {  
-    
+$(function () {
+
     // 取数据
-    function getData() {  
+    function getData() {
         let data = localStorage.getItem("todolist")
         if (data !== null) {
             return JSON.parse(data)
@@ -11,26 +11,26 @@ $(function () {
     }
 
     // 存数据
-    function saveData(data) {  
-        localStorage.setItem("todolist",JSON.stringify(data))
+    function saveData(data) {
+        localStorage.setItem("todolist", JSON.stringify(data))
     }
 
     // 渲染数据
-    function loadData() {  
+    function loadData() {
         let data = getData()
         let todonum = 0
         let donenum = 0
         $("ol, ul").empty()
-        $.each(data,function (i,ele) {
+        $.each(data, function (i, ele) {
             if (ele.done) {
-                $("ul").prepend("<li><input type='checkbox' checked='checked' > <p>" + ele.title + "</p> <a href='javascript:;' id="+i+"></a></li>");
+                $("ul").prepend("<li><input type='checkbox' checked='checked' > <p>" + ele.title + "</p> <a href='javascript:;' id=" + i + "></a></li>");
                 donenum++
-                
+
             } else {
-                $("ol").prepend("<li><input type='checkbox' > <p>" + ele.title + "</p> <a href='javascript:;' id="+i+"></a></li>");
+                $("ol").prepend("<li><input type='checkbox' > <p>" + ele.title + "</p> <a href='javascript:;' id=" + i + "></a></li>");
                 todonum++
             }
-            
+
         })
         $("#todocount").text(todonum);
         $("#donecount").text(donenum);
@@ -40,12 +40,12 @@ $(function () {
 
     // 1. 按下回车 把完整数据 存储到本地存储里面
     // 存储的数据格式  var todolist = [{title: "xxx", done: false}]
-    $("#title").on("keydown",function (ele) {  
+    $("#title").on("keydown", function (ele) {
         if (ele.keyCode === 13) {
             if ($(this).val() !== "") {
                 let local = getData()
                 // console.log(local);
-                local.push({title:$(this).val(),done:false})
+                local.push({ title: $(this).val(), done: false })
                 saveData(local)
                 $(this).val("")
             } else {
@@ -56,13 +56,13 @@ $(function () {
     })
 
     // 2、删除事件
-    $("ol, ul").on("click","a",function () {  
+    $("ol, ul").on("click", "a", function () {
         // 取数据
         let data = getData()
         console.log($(this));
         // 根据id删除
         let i = $(this).attr("id")
-        data.splice(i,1)
+        data.splice(i, 1)
         // 存储数据
         saveData(data)
         // 渲染数据
@@ -70,7 +70,7 @@ $(function () {
     })
 
     // 3、完成事件
-    $("ol, ul").on("click","input",function () {  
+    $("ol, ul").on("click", "input", function () {
         // 取数据
         let data = getData()
         // 判断done的值
